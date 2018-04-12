64a4c6bf3722595ba123a8f85223faad285b09f5
Ishikawa_N <naokingu0801@gmail.com>
2018-04-12 15:13:34 +0900

    Update spi_test.py

--- spi_test.py
+++ spi_test.py
@@ -1,7 +1,7 @@
 import spidev
-import time
+#import time
 
-usleep = lambda x : time.sleep(x/1000000.0)
+#usleep = lambda x : time.sleep(x/1000000.0)
 
 spi = spidev.SpiDev()
 spi.open(0, 1)
@@ -10,6 +10,6 @@ spi.max_speed_hz = 2000000
 while True:
     resp = spi.xfer2([0x00])    
     #print(resp[0])
-    usleep(1.0)
+    #usleep(1.0)
 
 spi.close()
